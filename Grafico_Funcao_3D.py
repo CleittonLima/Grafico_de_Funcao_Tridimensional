7fcec11a5f7300f43ce6bd8db5458faac7dfd216
Cleiton0Lima <cleittonlima135@gmail.com>
2023-03-11 11:56:38 -0300

    Initial commit

--- Grafico_Funcao_3D.py
+++ Grafico_Funcao_3D.py
@@ -1,24 +1,21 @@
-# Neccessário baixar as bibliotecas NUMPY, MATPLOTLIB.PYPLOT e MPL_TOOLKITS>MPLOT3D
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# Solicita que o usuário insira a função e o intervalo e se é raiz quadrada
+# solicita que o usuário insira a função e o intervalo
 # Se for elevado usa: **
 # Se a função for com o "e" = np.exp
 # Se a função for Sen = np.sin
 # Se a função for Cos = np.cos
 func_str = input("Insira a função (use x, y como variáveis): ")
-# Pergunta se a função está em raiz quadrada
 sqrt_input = input("A função contém raiz quadrada? (s/n)")
-# Pede o intervalo de X
 x_min, x_max = input("Insira o intervalo de X (ex: -10, 10): ").split(',')
-# Pede o intervalo de Y
 y_min, y_max = input("Insira o intervalo de Y (ex: -10, 10): ").split(',')
 x_min, x_max, y_min, y_max = float(x_min), float(x_max), float(y_min), float(y_max)
 
-# Caso a função seja em raiz
-# Define a função a ser plotada
+# converte a string do intervalo em uma tupla
+
+# define a função a ser plotada
 def f(x, y):
     if sqrt_input.lower() == 's':
         try:
@@ -31,26 +28,26 @@ def f(x, y):
         except:
             return np.nan
 
-# Cria os pontos para o eixo X e Y
+# cria os pontos para o eixo x e y
 x = np.linspace(x_min, x_max, 100)
 y = np.linspace(y_min, y_max, 100)
 X, Y = np.meshgrid(x, y)
 
-# Calcula os valores da função para cada ponto (x, y)
+# calcula os valores da função para cada ponto (x, y)
 Z = f(X, Y)
 
-# Cria o gráfico 3D
+# cria o gráfico 3D
 figura = plt.figure()
 ax = figura.add_subplot(111, projection='3d')
 ax.plot_surface(X, Y, Z, cmap='viridis')
 
-# Define os nomes dos eixos X, Y e Z
+# define os rótulos dos eixos
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 
-# Define o título do gráfico
+# define o título do gráfico
 plt.title(f'Gráfico 3D da função {func_str}')
 
-# Mostra o gráfico
+# mostra o gráfico
 plt.show()
